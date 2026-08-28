b997fde9b0969124b7987b15a8ec9d443a8bfe4a
MrTime <porsche.peaches2811@gmail.com>
2026-08-29 00:48:29 +0700

    Update SkibiDefenseCH4.lua

--- SkibiDefenseCH4.lua
+++ SkibiDefenseCH4.lua
@@ -149,7 +149,7 @@ if game.PlaceId == 14279724900 then
 
         -- 3. Game Status Check
         task.spawn(function()
-            while task.wait(5) do
+            while task.wait(30) do
                 -- Check HP
                 local hp = tonumber(HPLabel.Text:match("%d+"))
                 if hp and hp <= 0 then
